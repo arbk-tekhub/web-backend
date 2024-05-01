c9d9f5153de2c1a6b62589ee34681204af45175a
Arafet Ben Kilani <arafet.benkilani@supcom.tn>
2024-05-01 16:04:31 +0100

    Add Viper for parsing config

---
+++ internal/config/viper.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"errors"
+
+	"github.com/arbk-tekhub/www-backend/internal/validator"
+	"github.com/spf13/viper"
+)
+
+var config *viper.Viper
+
+func Load(configType, configFilePath string) error {
+
+	if !validator.PermittedValues(configType, "json", "yaml", "toml", "ini") {
+		return errors.New("config file type is not supported")
+	}
+
+	config = viper.New()
+	config.AutomaticEnv()
+	config.SetConfigType(configType)
+	config.SetConfigFile(configFilePath)
+
+	return config.ReadInConfig()
+
+}
+
+func Get() *viper.Viper {
+	return config
+}
