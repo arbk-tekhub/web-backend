62a3995c5493cfd1a1018e37bf38ed1fc2c0aa9d
Arafet Ben Kilani <arafet.benkilani@supcom.tn>
2024-05-03 22:32:30 +0100

    Add json field tags to the article model struct

--- internal/models/article.go
+++ internal/models/article.go
@@ -7,13 +7,13 @@ import (
 )
 
 type Article struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty"`
-	Title     string             `bson:"title"`
-	Content   string             `bson:"content,omitempty"`
-	Tags      []string           `bson:"tags,omitempty"`
-	Author    string             `bson:"author"`
-	Status    string             `bson:"status"`
-	Published time.Time          `bson:"published,omitempty"`
-	Created   time.Time          `bson:"created"`
-	Updated   time.Time          `bson:"updated,omitempty"`
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Title     string             `bson:"title" json:"title"`
+	Content   string             `bson:"content,omitempty" json:"content"`
+	Tags      []string           `bson:"tags,omitempty" json:"tags"`
+	Author    string             `bson:"author" json:"author"`
+	Status    string             `bson:"status" json:"status"`
+	Published time.Time          `bson:"published,omitempty" json:"published"`
+	Created   time.Time          `bson:"created" json:"created"`
+	Updated   time.Time          `bson:"updated,omitempty" json:"updated"`
 }
